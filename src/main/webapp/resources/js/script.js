873d10cf36ee74ce45dca8ad4c1655b92d89d4cb
emigu <emigu@192.168.0.17>
2018-10-21 18:50:25 -0300

    Final

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -7,14 +7,26 @@
 
 function validateLogin(){
     var email = document.forms["form"]["email"].value;
+    expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     if(email === ""){
         alert("El email es obligatorio!");
         return false;
+    }else{
+        if( !expr.test(email)){
+            alert("El email es incorrecto!");
+            return false;
+        }   
     }
     var password = document.forms["form"]["password"].value;
+    regex = /^(?=.*\d)(?=.*[a-záéíóúüñ]).*[A-ZÁÉÍÓÚÜÑ]/;
     if(password === ""){
         alert("La password es obligatoria!");
         return false;
+    }else{
+        if(password.length < 8 || !(regex.test(password))){
+            alert("El password es incorrecto!");
+            return false;
+        }
     }
     
     //Falta validar si el email es valido y que la contraseña tenga al menos 8 caracteres
