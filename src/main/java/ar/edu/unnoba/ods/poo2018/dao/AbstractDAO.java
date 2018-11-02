5e22b206e8713b4e95037a3f93f94563c817214b
bailsfrancisco <franciscobails4@gmail.com>
2018-11-02 20:31:08 -0300

    Commit 3 TP4

---
+++ src/main/java/ar/edu/unnoba/ods/poo2018/dao/AbstractDAO.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ar.edu.unnoba.ods.poo2018.dao;
+
+import javax.ejb.EJBException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author bailsfrancisco
+ */
+public abstract class AbstractDAO<T> {
+    final Class<T> typeParameter;
+
+    @PersistenceContext(unitName = "PU")
+    protected  EntityManager em;
+    
+    public AbstractDAO(Class<T> type){
+        this.typeParameter = type;
+    }
+    
+    public void create(T t) throws EJBException{
+        em.persist(t);
+    }
+    
+    public void update(T t) throws EJBException{
+        em.merge(t);
+    }
+    
+    public void delete(T t) throws EJBException{
+        em.remove(em.contains(t) ? t : em.merge(t));
+    }
+    
+    public T find(Long id){
+        return em.find(typeParameter, id);
+    }
+}
