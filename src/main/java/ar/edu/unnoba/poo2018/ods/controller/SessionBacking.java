a30af29fe729356ba4c3ab7263fd8a0c27257ea1
bailsfrancisco <franciscobails4@gmail.com>
2018-11-28 03:22:15 -0300

    TP5 FINAL: Internacionalizacion OK - Validacion e-mail OK

--- src/main/java/ar/edu/unnoba/poo2018/ods/controller/SessionBacking.java
+++ src/main/java/ar/edu/unnoba/poo2018/ods/controller/SessionBacking.java
@@ -3,11 +3,14 @@ package ar.edu.unnoba.poo2018.ods.controller;
 import ar.edu.unnoba.poo2018.ods.dao.UsuarioDAO;
 import ar.edu.unnoba.poo2018.ods.model.Usuario;
 import java.io.Serializable;
+import java.util.PropertyResourceBundle;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 
 @ManagedBean
 @SessionScoped
@@ -45,12 +48,19 @@ public class SessionBacking implements Serializable {
         this.usuario = usuario;
     }
 
+    @Inject
+    private transient PropertyResourceBundle bundle;
+
+    @PostConstruct
+    public void init() {
+    }
+
     public String login() {
         usuario = usuarioDAO.login(email, password);
         if (usuario == null) {
             //mensaje de error
             FacesContext context = FacesContext.getCurrentInstance();
-            FacesMessage message = new FacesMessage("Datos incorrectos");
+            FacesMessage message = new FacesMessage(bundle.getString("userLoginInvalid"));
             context.addMessage(null, message);
             return null;
         }
